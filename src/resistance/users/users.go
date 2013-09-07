0ac01d9771b28c9b819ce3dc21f5cbaff217f7bb
Andy Lau <hurrah@hurrah.(none)>
2013-09-07 01:18:16 -0700

    switched to use standard logger, implemented verfication of cookies and naive cookie generator

--- src/resistance/users/users.go
+++ src/resistance/users/users.go
@@ -1,6 +1,7 @@
 package users
 
 import (
+    "net/url"
     "net/http"
     "net/http/cookiejar"
 )
@@ -16,20 +17,39 @@ func ValidateUserCredentials(username string, password string) bool {
     return true
 }
 
-func ValidateUserCookie(cookies []*http.Cookie) bool {
+func ValidateUserCookie(url *url.URL, cookies []*http.Cookie) bool {
+    if len(cookies) == 0 {
+        return false
+    }
+
+    cookieJarCookies := cookieJar.Cookies(url)
+    var cookieJarCookieMap = make(map[string]bool)
+    for i := 0; i < len(cookieJarCookies); i++ {
+        cookieJarCookieMap[cookieJarCookies[i].String()] = true
+    }
+    
+    for i := 0; i < len(cookies); i++ {
+        if !cookieJarCookieMap[cookies[i].String()] {
+            return false
+        }
+    }
     return true
 }
 
 func ValidateUser(request *http.Request) (*http.Cookie, bool) {
     if len(request.Cookies()) > 0 {
-        validUser := ValidateUserCookie(request.Cookies())
+        validUser := ValidateUserCookie(request.URL, request.Cookies())
         if validUser {
             return nil, true
         }
-    } else if len(request.Form) > 0 {
-        validUser := ValidateUserCredentials(request.Form.Get(USERNAME_KEY), request.Form.Get(PASSWORD_KEY))
+    }
+    request.ParseForm()
+    if len(request.Form) > 0 {
+        username := request.FormValue(USERNAME_KEY)
+        password := request.FormValue(PASSWORD_KEY)
+        validUser := ValidateUserCredentials(username, password)
         if validUser {
-            cookie := generateNewCookie()
+            cookie := generateNewCookie(username)
             cookies := make([]*http.Cookie, 1)
             cookies[0] = cookie
             cookieJar.SetCookies(request.URL, cookies)
@@ -39,8 +59,8 @@ func ValidateUser(request *http.Request) (*http.Cookie, bool) {
     return nil, false
 }
 
-func generateNewCookie() *http.Cookie {
-    return nil
+func generateNewCookie(username string) *http.Cookie {
+    return &http.Cookie{Value: username}
 }
 
 func InitializeCookieJar() {
