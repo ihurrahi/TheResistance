7cca5b92c4424aea6a1357752d921b5079000454
Andy Lau <andylau1991@gmail.com>
2013-10-04 23:31:30 -0700

    implemented parsing generic messages

--- src/resistance/resistanceGAME/resistanceGAME.go
+++ src/resistance/resistanceGAME/resistanceGAME.go
@@ -9,21 +9,21 @@ import (
     "resistance/users"
 )
 
-type MessageHolder struct {
-    Message string
-    UserCookie string
-}
+const (
+    USER_COOKIE = "userCookie"
+    MESSAGE = "message"
+)
 
 // handlerPlayerDisconnect handles the message that is sent when
 // a player disconnects from the web socket proxy.
-func handlePlayerDisconnect(message *MessageHolder) string {
+func handlePlayerDisconnect(message map[string]interface{}) string {
     // TODO: implement
     return ""
 }
 
 // handlePlayerConnect handles the message that is sent when a player
 // first connects by loading the game page.
-func handlePlayerConnect(message *MessageHolder, user *users.User) string {
+func handlePlayerConnect(message map[string]interface{}, user *users.User) string {
     // TODO: implement sending a message to SUB zmqSocket
     utils.LogMessage(user.Username + " has sent a message!", utils.RESISTANCE_LOG_PATH)
     newMessage := "{\"message\":\"players\",\"acceptUser\":true,\"players\":[\"" + user.Username + "\"]}"
@@ -31,24 +31,22 @@ func handlePlayerConnect(message *MessageHolder, user *users.User) string {
 }
 
 // parseMessage parses every message that comes in and puts it into a Go struct.
-// TODO: make this generic to parse rest of the arguments
-func parseMessage(msg []byte) *MessageHolder {
-    var parsedMessage MessageHolder
+func parseMessage(msg []byte) map[string]interface{} {
+    var parsedMessage = make(map[string]interface{})
     
     utils.LogMessage(string(msg), utils.RESISTANCE_LOG_PATH)
     err := json.Unmarshal(msg, &parsedMessage)
     if err != nil {
         utils.LogMessage("Error parsing message: " + string(msg), utils.RESISTANCE_LOG_PATH)
-        return &MessageHolder{}
     }
     
-    return &parsedMessage
+    return parsedMessage
 }
 
 // getUser extracts the user from the parsed message returned from parseMessage().
-func getUser(parsedMessage *MessageHolder) *users.User {
+func getUser(parsedMessage map[string]interface{}) *users.User {
     cookies := make([]*http.Cookie, 1)
-    parsedCookie := strings.Split(parsedMessage.UserCookie, "=")
+    parsedCookie := strings.Split(parsedMessage[USER_COOKIE].(string), "=")
     cookies[0] = &http.Cookie{Name:parsedCookie[0], Value:parsedCookie[1]}
     user, success := users.ValidateUserCookie(cookies)
     if !success {
@@ -72,19 +70,19 @@ func main() {
     for {
         reply, _ := zmqSocket.Recv(0)
         parsedMessage := parseMessage(reply)
-        utils.LogMessage(parsedMessage.Message, utils.RESISTANCE_LOG_PATH)
+        utils.LogMessage(parsedMessage[MESSAGE].(string), utils.RESISTANCE_LOG_PATH)
         
         user := getUser(parsedMessage)
         
-        var message string
+        var returnMessage string
         switch {
-            case user == nil: message = ""
-            case parsedMessage.Message == "playerConnect": message = handlePlayerConnect(parsedMessage, user)
-            case parsedMessage.Message == "playerDisconnect": message = handlePlayerDisconnect(parsedMessage)
-            default: message = ""
+            case user == nil: returnMessage = ""
+            case parsedMessage[MESSAGE] == "playerConnect": returnMessage = handlePlayerConnect(parsedMessage, user)
+            case parsedMessage[MESSAGE] == "playerDisconnect": returnMessage = handlePlayerDisconnect(parsedMessage)
+            default: returnMessage = ""
         }
         
-        zmqSocket.Send([]byte(message), 0)
+        zmqSocket.Send([]byte(returnMessage), 0)
     }
 }
 
