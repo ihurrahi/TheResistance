d975b4ac4fee6fec574e53dcbcc0950156c81f06
Andy Lau <andylau1991@gmail.com>
2013-10-28 22:38:01 -0700

    realized the votes foreign key should be pointing towards the missions table

--- src/resistance/sql/10_votes.sql
+++ src/resistance/sql/10_votes.sql
@@ -1,8 +1,8 @@
 # Creates the votes table that stores the votes for each player for a specific team
 
 CREATE TABLE IF NOT EXISTS `votes` (
-  `team_id` BIGINT(20) NOT NULL,
+  `mission_id` BIGINT(20) NOT NULL,
   `user_id` BIGINT(20) NOT NULL,
   `vote` BIT(1) NOT NULL,
-  PRIMARY KEY(`team_id`, `user_id`)
+  PRIMARY KEY(`mission_id`, `user_id`)
 )
