3cdb8edd904e7945d57c95a129c67195cf27602a
Andy Lau <andylau1991@gmail.com>
2013-11-03 22:44:19 -0800

    modified teams table

--- src/resistance/sql/06_teams.sql
+++ src/resistance/sql/06_teams.sql
@@ -1,8 +1,7 @@
 # Describes the teams table - indicating which players went on which mission
 
 CREATE TABLE IF NOT EXISTS `teams` (
-  `team_id` BIGINT(20) NOT NULL AUTO_INCREMENT,
   `mission_id` BIGINT(20) NOT NULL,
   `user_id` BIGINT(20) NOT NULL,
-  PRIMARY KEY (`team_id`)
+  PRIMARY KEY (`mission_id`, `user_id`)
 )
